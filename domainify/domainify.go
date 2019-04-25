33c6f028fda416b7a070081f0e82616ed57f28b1
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-25 23:31:21 +0900

    ドメイン生成のコマンド

--- domainify/domainify.go
+++ domainify/domainify.go
@@ -1,1 +1,42 @@
-package domainify
+package main
+
+/*
+ドメインに利用できない文字を変換するコード
+末尾にトップレベルドメインを追加することが可能
+*/
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+// top level domeins
+var tlds = []string{"com", "cat"}
+
+// 使用可能な文字列
+const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+}
